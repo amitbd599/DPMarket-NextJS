b867d14de3a898a74ae6d05af5df98fb6e39d174
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-08-05 21:39:01 +0600

    contact page add

---
+++ master/src/app/contact/page.jsx
@@ -0,0 +1,49 @@
+import AllProduct from "@/components/AllProduct";
+import BrandSectionOne from "@/components/BrandSectionOne";
+import Breadcrumb from "@/components/Breadcrumb";
+import BreadcrumbEight from "@/components/BreadcrumbEight";
+import Contact from "@/components/Contact";
+import FooterOne from "@/components/FooterOne";
+import HeaderOne from "@/components/HeaderOne";
+import Newsletter from "@/components/Newsletter";
+import NewsletterTwo from "@/components/NewsletterTwo";
+import ResourceOne from "@/components/ResourceOne";
+import Preloader from "@/helper/Preloader";
+
+export const metadata = {
+  title: "Digital Market Place NEXT Js Template",
+  description:
+    "DpMarket – Digital Products Marketplace Html5 Template – A versatile and meticulously designed set of templates crafted to elevate your Digital Products Marketplace content and experiences.",
+};
+
+const page = () => {
+  return (
+    <>
+      {/* Preloader */}
+      <Preloader />
+
+      {/* HeaderOne */}
+      <HeaderOne />
+
+      {/* BreadcrumbEight */}
+      <BreadcrumbEight />
+
+      {/* Contact */}
+      <Contact />
+
+
+      {/* NewsletterTwo */}
+      <NewsletterTwo />
+
+
+      {/* BrandSectionOne */}
+      <BrandSectionOne />
+
+
+      {/* FooterOne */}
+      <FooterOne />
+    </>
+  );
+};
+
+export default page;
